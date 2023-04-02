7d4ec1595e68fab175e0413ab2f5e51f7f288f84
Muhammad Mehdi Ali <64282992+a-knaw-knee-mus@users.noreply.github.com>
2023-04-02 14:02:46 -0400

    add reactors on hover for each reaction

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,16 @@ function App() {
                         >
                             <div className="flex">
                                 <h5 className="text-md mr-2 font-semibold tracking-tight text-gray-900 dark:text-white">
-                                    <span className={message.creator?.email === "sbhadra@torontomu.ca" ? "text-red-500" : {}}>{message.creator?.name}</span>
+                                    <span
+                                        className={
+                                            message.creator?.email ===
+                                            "sbhadra@torontomu.ca"
+                                                ? "text-red-500"
+                                                : {}
+                                        }
+                                    >
+                                        {message.creator?.name}
+                                    </span>
                                 </h5>
                                 <p className="mt-[3px] text-sm font-light text-gray-500 dark:text-gray-400">
                                     {message?.created_date}
@@ -28,19 +37,34 @@ function App() {
                             </div>
                             <p className="font-normal text-gray-700 dark:text-gray-400">
                                 {message?.text}
-                                {message?.message_state && "message deleted by creator"}
+                                {message?.message_state &&
+                                    "message deleted by creator"}
                             </p>
                             <div className="mt-1 flex gap-2">
                                 {message.reactions?.map((reaction, index) => {
                                     return (
-                                        <div
-                                            key={index}
-                                            className="inline-flex items-center py-1 px-2 text-s text-center text-white bg-gray-300 rounded-lg focus:ring-4 focus:outline-none  dark:bg-slate-600 dark:focus:ring-slate-800"
-                                        >
-                                            {reaction.emoji.unicode}
-                                            <span className="inline-flex items-center justify-center w-4 h-4 ml-2 text-xs font-semibold text-blue-800 bg-gray-200 dark:bg-blue-200 rounded-full">
-                                                {reaction.reactor_emails.length}
-                                            </span>
+                                        <div className="group">
+                                            <div
+                                                key={index}
+                                                className="inline-flex items-center py-1 px-2 text-s text-center text-white bg-gray-300 rounded-lg focus:ring-4 focus:outline-none  dark:bg-slate-600 dark:focus:ring-slate-800"
+                                            >
+                                                {reaction.emoji.unicode}
+                                                <span className="inline-flex items-center justify-center w-4 h-4 ml-2 text-xs font-semibold text-blue-800 bg-gray-200 dark:bg-blue-200 rounded-full">
+                                                    {
+                                                        reaction.reactor_emails
+                                                            .length
+                                                    }
+                                                </span>
+                                            </div>
+                                            <div className="absolute sm:opacity-0 group-hover:opacity-100 transition-all bg-slate-600 rounded-md p-2 mt-2">
+                                                {reaction.reactor_emails.map((email, index) => {
+                                                    return (
+                                                        <p key={index} className="text-xs text-white">
+                                                            {email}
+                                                        </p>
+                                                    );
+                                                })}
+                                            </div>
                                         </div>
                                     );
                                 })}
@@ -49,8 +73,15 @@ function App() {
                     );
                 })}
             </div>
-            <a href="https://codebeautify.org/jsonviewer/y2318ef9e" target="_blank">
-                <Icon className="mx-auto pb-2" icon="material-symbols:frame-source" width={25} />
+            <a
+                href="https://codebeautify.org/jsonviewer/y2318ef9e"
+                target="_blank"
+            >
+                <Icon
+                    className="mx-auto pb-2"
+                    icon="material-symbols:frame-source"
+                    width={25}
+                />
             </a>
         </div>
     );
